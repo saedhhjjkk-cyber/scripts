34da35cb438fb711c4c3a256459c0ed5ad85474a
saedhhjjkk-cyber <saedhhjjkk@gmail.com>
2026-08-02 21:15:52 +0300

    [FNAN] Blox Fruits - 2026-08-02 18:15:46 UTC

---
+++ Blox Fruits/8c456ed3057b486f.lua
@@ -0,0 +1,28 @@
+-- FNAN Protected Script
+local _xckpejdk = (45 + 59) - 171
+if _xckpejdk ~= 4 then
+  print((99 + -57))
+
+  -- internal
+  local _qwmlkj = 389
+  if false then local _thqjrl = "_abaitmptfjmq" end
+  local _akwcju = 9872
+  if false then local _hntzbb = "_lfntlovcjdwe" end
+  local _tqqtam = ({rndInt(10, 500)} - 4) * 3
+  local _zvftxk = ({rndInt(10, 500)} - 4) * 1
+  local _dolnqe = type(49)
+  if _dolnqe == "string" then local _qgyva = _dolnqe end
+  if false then local _waokre = "_ukonhnxptxed" end
+  local _wcocoh = 4590
+  local _luqmwl = 2721
+  local _xidabj = type(40)
+  if _xidabj == "string" then local _lluwt = _xidabj end
+  local _rexjxp = 4831
+  local _wsprua = type(8)
+  if _wsprua == "string" then local _lzhhj = _wsprua end
+  local _hsppqm = type(17)
+  if _hsppqm == "string" then local _azjvz = _hsppqm end
+  local _yuxszm = ({rndInt(10, 500)} - 1) * 2
+  -- end internal
+
+end
